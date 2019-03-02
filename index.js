f824c73b58344e57ae3df3e80c54e515e6058037
bimal gurung <bimalgrg519@gmail.com>
2019-03-02 10:25:24 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>React Native Radio Button 2 Test</Text>
+    </View>
+  );
+};
+
+export default Index;

edc345bcf11baa97b2a3c39b47052ed55234e08f
bimal gurung <bimalgrg519@gmail.com>
2019-03-02 11:31:06 +0545

    source code added

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-const Index = () => {
-  return (
-    <View>
-      <Text>React Native Radio Button 2 Test</Text>
-    </View>
-  );
-};
+const RadioForm = ({ values, selectedValue, onPress, buttonColor }) => (
+  <View style={{ flexDirection: "row" }}>
+    {values.map(({ label, value }) => (
+      <TouchableOpacity
+        key={label}
+        style={{ flexDirection: "row", marginRight: 10 }}
+        onPress={() => onPress(value)}
+      >
+        <View
+          style={{
+            width: 18,
+            height: 18,
+            borderRadius: 9,
+            borderWidth: 2,
+            borderColor: buttonColor,
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 5
+          }}
+        >
+          {selectedValue === value && (
+            <View
+              style={{
+                width: 10,
+                height: 10,
+                borderRadius: 5,
+                backgroundColor: buttonColor
+              }}
+            />
+          )}
+        </View>
+        <Text style={{ color: "#ffffff", fontSize: 12 }}>{label}</Text>
+      </TouchableOpacity>
+    ))}
+  </View>
+);
 
-export default Index;
+export default RadioForm;
